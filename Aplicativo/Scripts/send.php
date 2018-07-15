dce47604953aacbe160289180b2682d386ba2e94
3LT Soluções Sustentáveis <39716371+3LT@users.noreply.github.com>
2018-07-15 18:59:01 -0300

    Update send.php

--- Aplicativo/Scripts/send.php
+++ Aplicativo/Scripts/send.php
@@ -1,27 +1,28 @@
 <?php
-	// Este script manda os ultimos dados armazenados
-	// do banco de dados para o aplicativo.
+	// Este script manda os dados dos sensores
+	// para a tela "Medições" do aplicativo.
 	$servidor='localhost';
 	$banco='measurer';
 	$usuario='root';
 	$senha='';
 	// Conectando e consutando o banco de dados.
 	$conn = new mysqli($servidor, $usuario, $senha, $banco);
-	// Checando conexão
+	// Checando a conexão.
 	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
+	    die("Conexão falhou!: " . $conn->connect_error);
 	} 
-	//Selecionando os valores do banco de dados.
+	// Selecionando os valores da base de dados.
 	$sql = "SELECT * FROM medidas ORDER BY id DESC LIMIT 1";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
-		while($row[] = $result->fetch_assoc()) {
-			// Armazenando os valores em JSON (JavaScript Object Notation - Notação de Objetos JavaScript).
-		    $json = json_encode($row);
-		}
+	    while($row[] = $result->fetch_assoc()) {
+		// Armazenando os valores em JSON (JavaScript Object Notation - Notação de Objetos JavaScript).
+		$json = json_encode($row);
+	    }
 	} else {
-		echo "0 resultados";
+	    echo "0 Resultados";
 	}
+	//Exibindo os valores de JSON.
 	echo $json;
 	//Encerrando a conexão.
 	$conn->close();
